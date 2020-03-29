de5c38718b89eece0c8c44e80d1cb5ffa4caca98
ManNguyen <7028401+ManNguyen@users.noreply.github.com>
2020-03-29 23:21:20 +0700

    update node-sass version

--- src/components/IndexDB/noteTemplate.js
+++ src/components/IndexDB/noteTemplate.js
@@ -19,7 +19,8 @@ let EmptyNote = {
 };
 
 
-let homePageBody = {"time":1579245849827,"blocks":[{"type":"header","data":{"text":" Welcome to Sakka!","level":1}},{"type":"paragraph","data":{"text":"<b>Sakka</b> is a note editor that stores your texts, documents in the web browser. There is no online storage involved in this application, all of your notes, poems, novel, memos, etc are stored locally in your machine.&nbsp;&nbsp;"}}],"version":"2.16.1"}
+let homePageBody = {"time":1579320948146,"blocks":[{"type":"header","data":{"text":" Welcome to Sakka!","level":1}},{"type":"paragraph","data":{"text":"<b>Sakka</b> is a note editor that stores your texts, documents in the web browser. There is <b>no online storage</b> involved in this application, all of your notes, poems, novel, memos, etc are stored locally in your machine.&nbsp;&nbsp;"}}],"version":"2.16.1"}
+
 let HomeNote =
 {
   title: "Home Page",
